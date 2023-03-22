30cad921329affb26d996047ca6233adb41dcec0
Thomas Henderson <tbhenderson55@yahoo.com>
2023-03-22 10:32:10 +0000

    v4

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,10 +1,24 @@
-import { Posts } from "./Posts";
-import "./App.css";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Posts } from './Posts';
+import './App.css';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     // provide React Query client to App
-    <div className="App">
+    <div className='App'>
+      <QueryClientProvider client={queryClient}>
+        {/* The rest of your application */}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
       <h1>Blog Posts</h1>
       <Posts />
     </div>
