e6bac0ba5e8085922dacaf2090e98b335a5eb216
Thomas Henderson <tbhenderson55@yahoo.com>
2023-03-31 12:25:36 +0100

    infinite scroll

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,14 +1,19 @@
-import "./App.css";
-import { InfinitePeople } from "./people/InfinitePeople";
-import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { InfinitePeople } from './people/InfinitePeople';
+// import { InfiniteSpecies } from './species/InfiniteSpecies';
+import './App.css';
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <div className='App'>
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,13 +1,47 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Person } from "./Person";
+import InfiniteScroll from 'react-infinite-scroller';
+import { Person } from './Person';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
-const initialUrl = "https://swapi.dev/api/people/";
+const initialUrl = 'https://swapi.dev/api/people/';
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isError,
+    error,
+    isFetching,
+  } = useInfiniteQuery(
+    ['sw-people'],
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+  );
+  if (isLoading) return <div className='loading'>Loading ...</div>;
+  if (isError) return <div className='error'>Error! {error.toString()}</div>;
+  return (
+    <>
+      {isFetching && <div className='loading'>Loading ...</div>}
+      <InfiniteScroll
+        loadMore={fetchNextPage}
+        hasMore={hasNextPage}
+        loader={<div key={0}>Loading ...</div>}
+      >
+        {data?.pages.map((page, i) => (
+          <ul key={i}>
+            {page.results.map((person) => (
+              <Person key={person.name} {...person} />
+            ))}
+          </ul>
+        ))}
+      </InfiniteScroll>
+    </>
+  );
 }
